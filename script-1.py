c6d441d9b4d27b31c28445d21205b6f4b19d4587
JoaquinMighetto <joaquinmighetto@gmail.com>
2024-11-01 21:56:27 -0300

    update requirements

--- script-1.py
+++ script-1.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import random, sys
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', passwd='1234', db='dannafox', charset = 'utf8mb4')
+cur = conn.cursor()
 
 
-def main(localidad : str, cantidad_a_generar : int = 7000):
+def main(localidad : str, cantidad_a_generar : int = 100):
 
     # Filtrar por localidad
     df = pd.read_excel("test.xls", sheet_name=1)
@@ -22,19 +26,50 @@ def main(localidad : str, cantidad_a_generar : int = 7000):
         # Tomar prefijos y bloques
     df_prefijo_bloque = df_primera_localidad[['BLOQUE', 'INDICATIVO']]
 
-    for index, numero in df_prefijo_bloque.iterrows():
-        print("+54", numero['INDICATIVO'], numero['BLOQUE'])
-        # generar_numero()
+    length_df = len(df_prefijo_bloque)
 
-        # Generar numeros
+    numeros_por_registro = cantidad_a_generar / length_df
+
+    listado_numeros = []
+
+    for index, numero in df_prefijo_bloque.iterrows():
+
+        bloque = numero['BLOQUE']
+        prefijo = numero['INDICATIVO']
+
+        for i in range(round(numeros_por_registro)):
+            if len(listado_numeros)==cantidad_a_generar: break
+
+            listado_numeros.append(generar_numero_telefono(prefijo, bloque))
+
+    agregar_a_la_db(listado_numeros)
+
 
     # Agregar a la db
+def agregar_a_la_db(lista : list):
+    
+    sql = "SELECT * FROM localidades WHERE "
+    localidad_id = cur.fetchone()
+        # Buscar id localidad
+
+            # Si no se encuentra Crear nueva localidad
+                # Obtener la nueva id
+
+            # Buscar la id
+
+            # Insertar en la db
+            
     
 
-def generar_numero(prefijo, bloque):
+def generar_numero_telefono(prefijo : str, bloque : str) -> str:
+    
+    resultado = str(prefijo) + str(bloque)
 
-    while len(prefijo) + len(bloque) < 10:
-        pass
+    for i in range(10 - len(resultado)):
+        resultado += ''.join(random.choice(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    ))
+    
+    return resultado
 
 
 if __name__ == '__main__':
